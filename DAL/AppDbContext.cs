bc77570576098f7d6b8500dc3835fe05302e254d
Bohdan <bohdan.romashchenko@gmail.com>
2020-01-15 15:33:10 +0200

    init with login

---
+++ DAL/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using DAL;
+using Microsoft.EntityFrameworkCore;
+
+namespace Domain
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Chat> Chats { get; set; } = default!;
+
+        public DbSet<Login> Logins { get; set; } = default!;
+
+        public DbSet<Message> Messages { get; set; } = default!;
+
+        public DbSet<User> Users { get; set; } = default!;
+
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ Domain/Chat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DAL
+{
+    public class Chat
+    {
+        public int ChatId { get; set; }
+
+        public ICollection<ChatUser> Users { get; set; } = default!;
+
+        public ICollection<Message>? Messages { get; set; }
+        
+    }
+}
