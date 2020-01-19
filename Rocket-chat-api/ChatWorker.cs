6e7f6dbeba0babb5d8962dbc870bfc8e067e813a
Bohdan <bohdan.romashchenko@gmail.com>
2020-01-20 00:10:02 +0200

    Can add new chat

--- Rocket-chat-api/ChatWorker.cs
+++ Rocket-chat-api/ChatWorker.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using DAL;
 using Domain;
 
@@ -14,21 +15,43 @@ namespace Rocket_chat_api
  
         private readonly AppDbContext _context;
         
-        public void AddChat(ICollection<User> users)
+        public int AddChat(ICollection<User> users)
         {
+
+            //TODO verify that such chat doesnt exist yet
+            
             var chatToAdd = new Chat();
             _context.Chats.Add(chatToAdd);
-            _context.SaveChanges();
-
+            
             foreach (var user in users)
-            {
-                _context.Add(new ChatUser
+            { 
+                _context.ChatUsers.Add(new ChatUser
                 {
                     User = user,
                     Chat = chatToAdd
                 });
+               
             }
             _context.SaveChanges();
+
+            return chatToAdd.ChatId;
         }
+        
+        /*
+        public bool ChatExists(int senderId, int receiverId)
+        {
+            var senderChatUser = _context.ChatUsers.FirstOrDefault(c=>c.User.UserId == senderId);
+            if(senderChatUser != null)
+            {
+                var workingChat = senderChatUser.Chat;
+                var receiverChatUser = _context.ChatUsers.Where(c=>c.UserId == receiverId).ToList();
+                if (receiverChatUser[0] != null)
+                {
+                    return workingChat == receiverChatUser[0].Chat;
+                }
+            }
+            return false;
+        }
+        */
     }
 }
---
+++ Rocket-chat-api/Controllers/ChatController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DAL;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Rocket_chat_api.Controllers
+{
+    [ApiController]
+    public class ChatController : ControllerBase
+    {
+
+        private readonly ILogger<LoginController> _logger;
+        
+        private readonly AppDbContext _context;
+        
+        private ChatWorker _chatWorker ;
+
+
+
+        public ChatController(ILogger<LoginController> logger,AppDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+            _chatWorker = new ChatWorker(_context);
+        }
+
+        [HttpGet]
+        [Route("/api/addchat")]
+        public IActionResult Login(int curUserId,string emailToAdd)
+        {
+            var chatId = -1;
+            var match =_context.Users.Where(u => u.Login.Email.Equals(emailToAdd)).ToList();
+            if (match.Count > 0)
+            {
+                var curUser = _context.Users.Find(curUserId);
+                chatId =_chatWorker.AddChat(new List<User>(){curUser,match[0]});
+                
+                return Ok(chatId);
+            }
+            return BadRequest("Email not found");
+        }
+    }
+}
